72dcc1425dc75843aa0d7dbfe9e49c84989e365d
Brijen Thananjeyan <brijenthananjeyan@gmail.com>
2016-07-15 20:39:47 -0700

    second commit

--- image_recorder.py
+++ image_recorder.py
@@ -40,16 +40,45 @@ def start_listening(interval=.01):
         os.makedirs(directory)
         os.makedirs(directory + "/left_endoscope")
         os.makedirs(directory + "/right_endoscope")
+    open(directory + "/psm1_sync.p", "w+").close()
+    open(directory + "/psm2_sync.p", "w+").close()
+
 
     imgsub = ImageSubscriber()
     time.sleep(4)
     count = 0
 
     while True:
+
         now = rospy.get_rostime()
-        scipy.misc.imsave(directory + "/left_endoscope/" + str(now) + '.jpg', imgsub.left_image)
-        scipy.misc.imsave(directory + "/right_endoscope/" + str(now) + '.jpg', imgsub.right_image)
-        count += 1
+        left = imgsub.left_image
+        right = imgsub.right_image
+
+        f = open(directory + "/psm1_sync.p", "a")
+        f2 = open(directory + "/psm2_sync.p", "a")
+        pose1 = psm1.get_current_cartesian_position()
+        pose2 = psm2.get_current_cartesian_position()
+        pos1 = pose1.position
+        pos2 = pose2.position
+        rot1 = [pose1.tb_angles.yaw_deg, pose1.tb_angles.pitch_deg, pose1.tb_angles.roll_deg]
+        rot2 = [pose2.tb_angles.yaw_deg, pose2.tb_angles.pitch_deg, pose2.tb_angles.roll_deg]
+        joint1 = psm1.get_current_joint_position()
+        joint2 = psm2.get_current_joint_position()
+        masterpose1 = psm1.get_master_position_cartesian_current()
+        masterpose2 = psm1.get_master_position_cartesian_current()
+        masterjoint1 = psm1.get_master_joint_current()
+        masterjoint2 = psm1.get_master_joint_current()
+        grip1 = [joint1[-1] * 180 / np.pi]
+        grip2 = [joint2[-1] * 180 / np.pi]
+        one = [now] + list(pos1) + rot1 + list(grip1) + list(joint1) + list(masterpose1) + list(masterjoint1)
+        two = [now] + list(pos2) + rot2 + list(grip2) + list(joint2) + list(masterpose2) + list(masterjoint2)
+
+        scipy.misc.imsave(directory + "/left_endoscope/" + str(now) + '.jpg', left)
+        scipy.misc.imsave(directory + "/right_endoscope/" + str(now) + '.jpg', right)
+
+        pickle.dump(one, f)
+        pickle.dump(two, f2)
+
         time.sleep(interval)
 
 if __name__ == '__main__':
