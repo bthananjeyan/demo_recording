51e59147471dbf208e6309d4df458920ad51f3d9
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2016-07-23 14:56:39 -0700

    added .gitignore

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 if __name__ == "__main__":
-	demos =  [demo for demo in os.listdir(os.getcwd()) if (demo != '.git' and demo != 'preprocessing' and demo != 'recording' and demo[-2:] != "py")]
+	demos =  [demo for demo in os.listdir(os.getcwd()) if (demo != '.git' and demo != 'preprocessing' and demo != 'recording' and demo[-2:] != "py" and demo[-3:] != "pyc")]
 	sys.path.insert(0, os.getcwd() + '/preprocessing')
 	from process import *
 
