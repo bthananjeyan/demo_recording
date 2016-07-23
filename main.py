8f64f5e6b242d04a6e541ef345f9f75d171ae7b7
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2016-07-23 14:52:04 -0700

    added main

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import sys
+import os
+
+if __name__ == "__main__":
+	demos =  [demo for demo in os.listdir(os.getcwd()) if (demo != '.git' and demo != 'preprocessing' and demo != 'recording' and demo[-2:] != "py")]
+	sys.path.insert(0, os.getcwd() + '/preprocessing')
+	from process import *
+
+	for demo in demos:
+		process_demo(demo)
--- preprocessing/process.py
+++ preprocessing/process.py
@@ -5,8 +5,12 @@ import sys
 def process_demo(demo_directory):
 	# Load data
 	with h5py.File("../" + demo_directory + '/data.h5','r') as hf:
-		psm1_sync = np.array(hf.get('psm1_sync'))
-		psm2_sync = np.array(hf.get('psm2_sync'))
+		psm1_sync = np.matrix(hf.get('psm1_sync'))
+		psm2_sync = np.matrix(hf.get('psm2_sync'))
+		transform = np.matrix(hf.get('camera_to_robot'))
+	invtransform = np.zeros(transform.shape)
+	invtransform[:3, :3] = transform[:3,:3].T
+	invtransform[:, 3] = -tranform[:,3]
 	# Fix times
 	temp = np.ones((psm1_sync.shape[0], 1)) * psm1_sync[0,0]
 	psm1_sync[:,0] -= temp
@@ -16,6 +20,9 @@ def process_demo(demo_directory):
 	psm1_sync = psm1_sync[:, good_features]
 	psm2_sync = psm2_sync[:, good_features]
 	return psm1_sync, psm2_sync
+
+def transform_matrix(data, transform):
+	return np.hstack((data, np.ones((data.shape[0], 1)))) * transform.T
 
 if __name__ == "__main__":
 
