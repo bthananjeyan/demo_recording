5874f8ba50532ce5008cdd51ea6f5a2000ff6a1a
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2016-07-23 12:39:26 -0700

    basic cleaning

--- preprocessing/process.py
+++ preprocessing/process.py
@@ -3,7 +3,19 @@ import h5py
 import sys
 
 def process_demo(demo_directory):
-	return
+	# Load data
+	with h5py.File("../" + demo_directory + '/data.h5','r') as hf:
+		psm1_sync = np.array(hf.get('psm1_sync'))
+		psm2_sync = np.array(hf.get('psm2_sync'))
+	# Fix times
+	temp = np.ones((psm1_sync.shape[0], 1)) * psm1_sync[0,0]
+	psm1_sync[:,0] -= temp
+	psm2_sync[:,0] -= temp
+	# Throw out bad features
+	good_features = np.r_[0:psm1_sync.shape[1]]
+	psm1_sync = psm1_sync[:, good_features]
+	psm2_sync = psm2_sync[:, good_features]
+	return psm1_sync, psm2_sync
 
 if __name__ == "__main__":
 
