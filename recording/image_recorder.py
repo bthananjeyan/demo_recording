625e007dbbdcd7d033a8fcdb0a4cdb19d018c78e
Sanjay Krishnan <takeshidanny@gmail.com>
2018-05-18 15:42:04 -0700

    cleanup

--- recording/image_recorder.py
+++ recording/image_recorder.py
@@ -5,12 +5,20 @@ from multiprocessing import Process
 import os
 from robot import *
 import numpy as np
-import IPython
 from ImageSubscriber import ImageSubscriber
 import scipy
-import matplotlib.pyplot as plt
 import sys
 import multiprocessing
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
+
+current_force_capacitor_value = np.array([1., 0.], dtype=np.float32)
+
+def force_callback(msg):
+
+    current_force_capacitor_value =  msg.data
+    print(current_force_capacitor_value)
+
 def startCallback():
     global record_process, exit
     print "start"
@@ -67,19 +75,21 @@ def start_listening(exit, interval=.04):
     imgsub = ImageSubscriber()
 
     imgsub.dump_camera_info(directory + "/camera_info.p")
-    f = open("camera_to_robot.p", "rb")
-    cmat = pickle.load(f)
-    f.close()
-    f = open(directory + "/camera_to_robot.p", "w+")
-    pickle.dump(cmat, f)
-    f.close()
+    with open("camera_to_robot.p", "rb") as f:
+        cmat = pickle.load(f)
+    with open(directory + "/camera_to_robot.p", "w+") as f:
+        pickle.dump(cmat, f)
 
+    rospy.Subscriber("/force/capsense", numpy_msg(Floats), force_callback, queue_size=1)
 
     time.sleep(2)
     count = 0
     
     left_images = {}
     right_images = {}
+
+    f = open(directory + "/psm1_sync.p", "a")
+    f2 = open(directory + "/psm2_sync.p", "a")
 
     while not exit.is_set():
 
@@ -89,8 +99,7 @@ def start_listening(exit, interval=.04):
         left = imgsub.left_image
         right = imgsub.right_image
 
-        f = open(directory + "/psm1_sync.p", "a")
-        f2 = open(directory + "/psm2_sync.p", "a")
+        
         pose1 = psm1.get_current_cartesian_position()
         pose2 = psm2.get_current_cartesian_position()
         pos1 = pose1.position
@@ -105,8 +114,8 @@ def start_listening(exit, interval=.04):
         masterjoint2 = psm1.get_master_joint_current()
         grip1 = [joint1[-1] * 180 / np.pi]
         grip2 = [joint2[-1] * 180 / np.pi]
-        one = [now] + list(pos1) + rot1 + list(grip1) + list(joint1) + list(masterpose1) + list(masterjoint1)
-        two = [now] + list(pos2) + rot2 + list(grip2) + list(joint2) + list(masterpose2) + list(masterjoint2)
+        one = [now] + list(pos1) + rot1 + list(grip1) + list(joint1) + list(masterpose1) + list(masterjoint1) + list(current_force_capacitor_value)
+        two = [now] + list(pos2) + rot2 + list(grip2) + list(joint2) + list(masterpose2) + list(masterjoint2) + list(current_force_capacitor_value)
 
 
         left_images[directory + "/left_endoscope/" + str(now) + '.jpg'] = left
